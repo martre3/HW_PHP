11495cc386643851c5192c389b89ad8e95c3867c
Martynas Treinys <martynas7928@gmail.com>
2017-10-14 17:28:58 +0300

    __set and __get methods added

--- Demo.php
+++ Demo.php
@@ -2,11 +2,20 @@
 
 require_once(__DIR__ . '/vendor/autoload.php');
 
+// testing __construct methods
 $obj1 = new Top\Magic();
 $obj2 = new Deep\First\Magic();
 $obj3 = new Deep\Second\Magic();
+echo PHP_EOL;
 
-$obj1->inaccessibleMethod();//testing __call method by trying to access private method
+$obj1->inaccessibleMethod("public");//testing __call method by trying to access private method
+$obj1::notExisting();//testing __callStatic method by trying to access not existing method
+echo PHP_EOL;
+
+$obj1->unknown = "some value";//testing __set method by setting value to variable that has not been declared
+echo "returned value: '" . $obj1->unknown . "'" . PHP_EOL;//testing __get method by accessing previously created variable
+echo PHP_EOL;
+
 
 
 
--- src/magic/Magic.php
+++ src/magic/Magic.php
@@ -4,6 +4,7 @@ namespace Top;
 
 class Magic
 {
+    private $data = array();
     private $name;
     private $hidden;
 
@@ -20,11 +21,32 @@ class Magic
 
     public function __call($name, $arguments)
     {
-        echo "Sorry, but " . $name . " is private or doesn't exist" . PHP_EOL;
+        echo "Sorry, but " . $name . "(". implode(', ', $arguments). ") method is private or doesn't exist" . PHP_EOL;
     }
 
-    private function inaccesibleMethod()
+    public static function __callStatic($name, $arguments)
     {
-        echo "Unreachable";
+        echo "Sorry, but static " . $name . "(". implode(', ', $arguments). ") method is private or doesn't exist" . PHP_EOL;
     }
+
+    private function inaccessibleMethod($args)
+    {
+        echo "This is not accessible" . PHP_EOL;
+    }
+
+    public function __set($name, $value)
+    {
+        echo "'" . $name . "' has been created with '" . $value . "'" . PHP_EOL;
+        $this->data[$name] = $value;
+    }
+
+    public function __get($name)
+    {
+        if (array_key_exists($name, $this->data)) {
+            echo "'$name' successfully returned" . PHP_EOL;
+            return $this->data[$name];
+        }
+        echo "No property '" .$name . "' in object " . $this->name . PHP_EOL;
+    }
+
 }
